09c3640803d40b26c6aa706af84ea8f067ea2593
Md. Farhan Ishtiyak Sezar <farhanishtiyaksezar@gmail.com>
2022-09-01 02:31:44 +0600

    Footbll Problem Solved

--- Codeforces/Football.cpp
+++ Codeforces/Football.cpp
@@ -1,9 +1,28 @@
+//problem link : https://codeforces.com/problemset/problem/43/A
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int i=0;
-    int x = i%2;
-    cout<<x<<end;
+    int n;
+    cin>>n;
+    unordered_map<string,int> ans;
+    for(int i=0; i<n; i++) 
+    {
+        string s;
+        cin>>s;
+        ans[s]++;
+    }
+    int maximum = INT_MIN;
+    for(auto value = ans.begin(); value !=ans.end(); ++value)
+    {
+        maximum=max(maximum,value->second);
+    }
+    
+    for(auto value = ans.begin(); value !=ans.end(); ++value)
+    {
+        if(value->second==maximum) cout<<value->first<<endl;
+    }
+    
 }
