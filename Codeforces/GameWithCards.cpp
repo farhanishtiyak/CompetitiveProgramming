7969bcea0e7accc3d1b3832d00e12a54db1008f5
Md. Farhan Ishtiyak Sezar <farhanishtiyaksezar@gmail.com>
2022-09-02 02:20:04 +0600

    Game With Card.cpp

---
+++ Codeforces/GameWithCards.cpp
@@ -0,0 +1,44 @@
+// Problem link: https://codeforces.com/contest/1681/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solution ()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    vector<int>vec;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        //if(x>alice) alice=x;
+        v.push_back(x);
+    }
+    int m;
+    cin>>m;
+    for(int i=0; i<m; i++)
+    {
+        int x;
+        cin>>x;
+        //if(x>bob) bob=x;
+        vec.push_back(x);
+    }
+    int a = *max_element(v.begin(),v.end());
+    int b = *max_element(vec.begin(),vec.end());
+    if(a>b)
+    {
+        cout<<"Alice"<<endl<<"Alice"<<endl;
+    }
+    else if(b>a) cout<<"Bob"<<endl<<"Bob"<<endl;
+    else cout<<"Alice"<<endl<<"Bob"<<endl;
+
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    solution();
+}
