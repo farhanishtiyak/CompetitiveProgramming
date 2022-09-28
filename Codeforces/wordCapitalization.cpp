9396c5fb7a9a5ea64fce44de3ea42ec69701010e
Md. Farhan Ishtiyak Sezar <farhanishtiyaksezar@gmail.com>
2022-09-28 23:08:11 +0600

    Created

---
+++ Codeforces/wordCapitalization.cpp
@@ -0,0 +1,12 @@
+// Problem Link : https://codeforces.com/problemset/problem/281/A
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string s;
+    cin>>s;
+    s[0] = s[0] & '_';
+    cout<<s<<endl;
+}
